44b7d7c77a974c693f2eb3413751b7e6587daaa1
김수빈 <sbkim099@naver.com>
2026-05-08 16:42:43 +0900

    Board List 출력 및 페이지 네비게이터 구현

--- src/main/java/com/study/app/domains/board/BoardDAO.java
+++ src/main/java/com/study/app/domains/board/BoardDAO.java
@@ -1,5 +1,27 @@
 package com.study.app.domains.board;
 
-public class BoardDAO {
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BoardDAO {
+	
+	@Autowired
+	private SqlSessionTemplate mybatis;
+	
+	public int countAllPosts() {
+		return mybatis.selectOne("Board.countAllPosts");
+	}
+	
+	public List<BoardDTO> getStartEnd(Long start, Long end){
+		Map<String, Long> param = new HashMap<>();
+		param.put("start", start);
+		param.put("end", end);
+		return mybatis.selectList("Board.getStartEnd", param);
+	}
 }
